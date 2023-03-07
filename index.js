445c5923337cd42ebee6ae50e8fe66e06f6ee21c
Harshita <harshitalakhchaura@gmail.com>
2023-03-07 22:09:02 +0530

    added io connection code

--- index.js
+++ index.js
@@ -9,4 +9,30 @@ const io=require('socket.io')(server,{
     }
 })
 
-app.use(cors);
+app.use(cors());
+
+const PORT=process.env.PORT || 5000;
+
+app.get("/",(req,res)=>{
+    res.send('Server is running');
+});
+
+io.on('connection',(socket)=>{
+    socket.emit('me', socket.id)
+
+    socket.on('disconnect',()=>{
+        socket.broadcast.emit('callended');
+    })
+
+    socket.on('calluser',({userToCall, signalData, from, name})=>{
+        io.to(userToCall).emit('calluser',{signal:signalData, from, name})
+    })
+
+    socket.on('answercall',(data)=>{
+        io.to(data).emit('callaccepted', data.signal)
+    })
+})
+
+server.listen(PORT, ()=>{
+    console.log('listening on port '+PORT);
+})
